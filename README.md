4fdc3b741c586b88dfb3c5aa4a9ae1d1a25c9b42
ramasalahat <ramasalahat@gmail.com>
2020-12-22 00:13:25 +0200

    documentation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# training-queries-elasticsearch
+# Creating Queries with Elasticsearch
+
+The code in this repository aims to do the following: 
+- combine multiple csv files that have data about places in NewYork city,
+- creat queries out of the places
+- connect to the Elasticsearch instant and send requests for each query 
+- export the resulting data as a json file
--- es_data.py
+++ es_data.py
@@ -5,20 +5,40 @@ import json
 
 
 def remove_duplicates(my_list):
+    '''
+    remove duplicates from list
+    inputs: my_list: list to be cleaned from duplicates
+    outputs: cleaned list
+    '''
     return list(dict.fromkeys(my_list))
 
 
 def extract_queries(datasets):
+    '''
+    extract random queries from the places names
+    inputs: datasets: dataset of places
+    output: quereies list
+    '''
+    #take the names of places and put them in a list 
     places = pd.concat(datasets)['name'].tolist()
+    #remove duplicates
     unique_places = remove_duplicates(places)
+    #delete places with length of string less than 3
     cleaned_places = [place for place in unique_places if len(str(place)) > 3]
+    #take renadom sub parts of size 3 to 10 of the places names
     init_queries = list(map(lambda x: x[:(random.randint(3, 10))].strip(), cleaned_places))
+    #remove the characters @ # %
     remove_table = str.maketrans('', '', '@#%')
+    #combine the results in one dataframe
     cleaned_list = [s.translate(remove_table) for s in init_queries]
     final_queries = remove_duplicates(cleaned_list)
     return final_queries
 
+'''
+main entry point
 
+
+'''
 if __name__ == '__main__':
     # Reading data
     restaurants = pd.read_csv('restaurant_point.csv', usecols=['name'])
@@ -32,6 +52,7 @@ if __name__ == '__main__':
     results = {}
     count = 0
     for query in queries[10000:]:
+        #get records from elasticsearch
         response = requests.get(
             'https://query-manager.herokuapp.com/search?query=' + query + '&lon=40.7128&lat=74.0060')
         json_data = list(json.loads(response.text))[0:5]
